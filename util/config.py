b1f193b8ecc896e999862e7d20b1d4a49ada149c
Cesar Rodriguez <cesar.idaho@gmail.com>
2019-05-11 22:16:36 +0200

    More pythonic configuration checking

--- util/config.py
+++ util/config.py
@@ -3,13 +3,15 @@
 import configparser
 import logging
 
+
 class ConfigClass:
     """ Class for controlling configurations"""
     _CONFIG_FILE = 'test/testconfig.ini'
     parser = None
 
-    def __init__(self, configfile=''):
-        if configfile != '':
+    def __init__(self, configfile=None):
+        logging.debug("Received configuration file is: " + str(configfile))
+        if configfile is not None:
             self._CONFIG_FILE = configfile
 
         self.parser = configparser.ConfigParser()
@@ -18,7 +20,3 @@ class ConfigClass:
         except:
             logging.critical("Unexpected error:", sys.exc_info()[0])
             raise
-
-
-
-
